de2b1ad844a6b321bb8ceb72db3cec2d7ddd576f
BCT-107 <79015222+BCT-107@users.noreply.github.com>
2021-02-20 01:58:53 +0000

    Imported UI stuff
    Bear with the current crosshair

--- Source/ProjectSimulation/ProjectSimulationHUD.cpp
+++ Source/ProjectSimulation/ProjectSimulationHUD.cpp
@@ -10,7 +10,7 @@
 AProjectSimulationHUD::AProjectSimulationHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPersonCPP/UI/UI_Assets_/Crosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
